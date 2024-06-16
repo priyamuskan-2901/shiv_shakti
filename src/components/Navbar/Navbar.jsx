a8f26f288ca33ed6b4b05b95f99eb273f0e5dcaf
Sumit sahu <Sumit.sahu.civ22@ithu.ac.in>
2024-06-17 02:57:20 +0530

    updated nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,9 +4,14 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
   };
 
   return (
@@ -26,13 +31,47 @@ export const Navbar = () => {
             <Link to="/">Home</Link>
           </li>
           <li className={styles.menuitem}>
-            <Link to="about">About Us</Link>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li
+            className={`${styles.menuitem} ${styles.dropdown}`}
+            onClick={toggleDropdown}
+          >
+            <span>
+              <Link to="/companies">Companies</Link>
+              <span
+                className={`${styles.dropdownArrow} ${
+                  isDropdownOpen ? styles.open : ""
+                }`}
+              >
+                ▼
+              </span>
+            </span>
+            <ul
+              className={`${styles.dropdownMenu} ${
+                isDropdownOpen ? styles.show : ""
+              }`}
+            >
+              <li className={styles.dropdownItem}>
+                <Link to="/explore/OM SHIV FARMS">Om Shiv Farms</Link>
+              </li>
+              <li className={styles.dropdownItem}>
+                <Link to="/explore/SHIV ANITA FARMS">Shiv Anita Farms</Link>
+              </li>
+              <li className={styles.dropdownItem}>
+                <Link to="/explore/SHIV SHAKTI ENTERPRISES">
+                  Shiv Shakti Enterprises
+                </Link>
+              </li>
+              <li className={styles.dropdownItem}>
+                <Link to="/explore/SHIV SHAKTI FEED MILS">
+                  Shiv Shakti Feed Mils
+                </Link>
+              </li>
+            </ul>
           </li>
           <li className={styles.menuitem}>
-            <Link to="/companies">Companies</Link>
-          </li>
-          <li className={styles.menuitem}>
-            <Link to="contact">Contact Us</Link>
+            <Link to="/contact">Contact Us</Link>
           </li>
         </ul>
       </div>
